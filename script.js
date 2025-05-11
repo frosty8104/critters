7c674b37c949d1a8c06fae63a7be39b2432a186b
laura <lg8104@gmail.com>
2025-05-10 19:23:28 -0600

    Updating critters page

--- script.js
+++ script.js
@@ -1,43 +1,89 @@
-document.addEventListener('DOMContentLoaded ', function(){
+document.addEventListener('DOMContentLoaded', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 500;
     canvas.height = 800;
     class Game {
-        constructor(){
+        constructor(ctx, width, height){
+            this.ctx = ctx;
+            this.width = width;
+            this.height = height;
             this.enemies = [];
-            this.addNewEnemies();
+            this.enemyInterval = 100;
+            this.enemyTimer = 0;
+            this.enemyTypes = ['worm', 'ghost'];
         }
-        update(){
-            
+        update(deltaTime){
+            this.enemies = this.enemies.filter(object => !object.markedForDeletion);
+            if (this.enemyTimer > this.enemyInterval){
+                this.#addNewEnemy();
+                this.enemyTimer = 0;
+                console.log(this.enemies);
+            } else {
+                this.enemyTimer += deltaTime;
+            }
+            this.enemies.forEach(object => object.update());
         }
         draw(){
-            
+            this.enemies.forEach(object => object.draw(this.ctx));
         }
         #addNewEnemy(){
-            this.enemies.push(new Enemy());
+            const randomEnemy = this.enemyTypes[Math.floor(Math.random() * this.enemyTypes.length)];
+            if (randomEnemy == 'worm') this.enemies.push(new Worm (this));
+            else if (randomEnemy == 'ghost') this.enemies.push(new Ghost (this));
+            this.enemies.sort(function(a,b){
+                return a.y - b.y;
+            });
         }
     }
     class Enemy {
-        constructor(){
-            this.x = 100;
-            this.y = 100;
-            this.width = 100;
-            this.height = 100;
+        constructor(game){
+            this.game = game;
+            this.markedForDeletion = false;
         }
-        update(){
-            this.x--;
+        update(deltaTime){
+            this.x -= this.vx * deltaTime;
+            if (this.x < 0 - this.width) this.markedForDeletion = true;
         }
-        draw(){
-            ctx.fillRect(this.x, this.y, this.width, this.height)
+        draw(ctx){
+            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
         }
     }
+    class Worm extends Enemy {
+        constructor(game){
+            super(game);
+            this.spriteWidth = 229;
+            this.spriteHeight = 171;
+            this.width = this.spriteWidth/2;
+            this.height = this.spriteHeight/2;
+            this.x = this.game.width;
+            this.y = Math.random() * this.game.height;
+            this.image = worm;
+            this.vx = Math.random() * 0.1 + 0.1; 
+        }
+    }
+    class Ghost extends Enemy {
+        constructor(game){
+            super(game);
+            this.spriteWidth = 261;
+            this.spriteHeight = 209;
+            this.width = this.spriteWidth/2;
+            this.height = this.spriteHeight/2;
+            this.x = this.game.width;
+            this.y = Math.random() * this.game.height;
+            this.image = ghost;
+            this.vx = Math.random() * 0.2 + 0.1; 
+        }
+    }
+    const game = new Game(ctx, canvas.width, canvas.height);
     let lastTime = 1;
     function animate(timeStamp){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         const deltaTime = timeStamp - lastTime;
         lastTime = timeStamp;
+        game.update(deltaTime);
+        game.draw();
         requestAnimationFrame(animate); 
     }
     animate(0);
-}); 
+});
